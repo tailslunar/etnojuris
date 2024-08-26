ef15989ba78645edce4f683bda1aec67ba433d5b
tailslunar <tailslunar@gmail.com>
2024-08-26 18:19:32 -0300

    arrumando modelos

--- app/Models/estados.php
+++ app/Models/estados.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class estados extends Model
+class Estados extends Model
 {
     use HasFactory;
 
